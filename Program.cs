a52f240cd291265b34594f00f3a7072a1d5f9528
1234 <gdksgjfjffhgjghf@gmail.com>
2024-03-13 14:50:48 +0500

    Code Refactor

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
 // ---------------------------------------------------------------
 using System.ComponentModel;
 
+
 DavomEt:
 System.Console.WriteLine("Welcome to my project!");
 System.Console.WriteLine("===========Main=========,===========Drawing=========");
@@ -12,9 +13,11 @@ string Select = Console.ReadLine();
 
 if(Select == "1")
 {
+
     System.Console.WriteLine("What do you need?");
     System.Console.WriteLine("The volume of the cube: ======= The perimeter of a rectangle: ======= The perimeter of the triangle");
     string Choose = Console.ReadLine();
+
 
     if(Choose == "1")
     {
@@ -27,17 +30,24 @@ if(Select == "1")
       int balandligi = Convert.ToInt32(Console.ReadLine());
       System.Console.WriteLine($"Volume of the cube: {eni * buyi * balandligi}");
     }
+
    else if(Choose == "2")
+
     {
+
         System.Console.WriteLine("Enter the sides of the rectangle: ");
         System.Console.Write("A: ");
         int A = Convert.ToInt32(Console.ReadLine());
         System.Console.Write("B: ");
         int B = Convert.ToInt32(Console.ReadLine());
         System.Console.WriteLine($"Primetr: {(A + B) * 2}");
+
     }
+
     if(Choose == "3")
+
     {
+
         System.Console.WriteLine("Enter the sides of the triangle:");
         System.Console.Write("A: ");
         int A = Convert.ToInt32(Console.ReadLine());
@@ -46,36 +56,60 @@ if(Select == "1")
         System.Console.Write("C: ");
         int C = Convert.ToInt32(Console.ReadLine());
         System.Console.WriteLine($"The perimeter of the triangle: {A + B + C}");
+
     }
+
 }
 
+
 if(Select == "2")
+
 {
+
     System.Console.WriteLine("What do you want to draw?");
     System.Console.WriteLine("A picture of a rectangle:=========Square picture:========== A picture of a triangle:");
-    System.Console.Write("Nima kerak: ");
+    System.Console.Write("What do you need?");
     string What = Console.ReadLine();
+
 
     if(What == "1")
     {
+
         System.Console.WriteLine("**********\n*        *\n*        *\n*        *\n*        *\n**********\n");
+
     }
+
     if(What == "2")
+
     {
+
          Console.WriteLine("**********\n*        *\n*        *\n*        *\n**********\n");
+
     }
+
     if(What == "3")
+
     {
          Console.WriteLine("      *\n     *  *\n    *    *\n   *      *\n  *        *\n *          *\n**************");
+
     }
+
 }
+
 System.Console.WriteLine("Will you continue?");
 string Tanlang = Console.ReadLine();
 if(Tanlang == "Yes")
+
 {
+
     goto DavomEt;
+
 }
+
 else if(Tanlang == "No")
+
 {
+
     System.Console.WriteLine("We say goodbye to you!");
+    
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Geometry
-I hope you can perform geometric operations with it and find it useful
+ I hope you can perform geometric operations with it and find it useful
+ - ## Code run
+ ![Alt text](image.png)
